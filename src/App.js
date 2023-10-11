332af47e0e72baff0e1b58c249cb263628be0648
rokaf340 <rokaf340@gmail.com>
2023-10-11 23:47:54 +0900

    buttonTest

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [diasbled, setDiasbled] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +12,7 @@ function App() {
           type="button"
           data-testid="minus-button"
           onClick={() => setCount((prev) => prev - 1)}
+          disabled={diasbled}
         >
           {/* setState는 비동기로 작동한다. = state의 값은 setState가 호출되는 시점이 아닌, 해당 코드가 들어있는 함수가 모두 실행된 이후에 바뀐다.
           => 임시공간에 있는값을 사용하기위해 prev(임시공간) 사용해야 올바른 값이 나옴 */}
@@ -20,6 +22,7 @@ function App() {
           type="button"
           data-testid="plus-button"
           onClick={() => setCount((prev) => prev + 1)}
+          disabled={diasbled}
         >
           +
         </button>
@@ -28,6 +31,7 @@ function App() {
             type="button"
             style={{ backgroundColor: "blue" }}
             data-testid="on/off-button"
+            onClick={() => setDiasbled((prev) => !prev)}
           >
             on/off
           </button>
