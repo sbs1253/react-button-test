4882e741f661819fb7d3dc6f9f7ab9c6b2a59c64
rokaf340 <rokaf340@gmail.com>
2023-10-11 23:18:12 +0900

    testfile

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
           data-testid="minus-button"
           onClick={() => setCount((prev) => prev - 1)}
         >
+          {/* setState는 비동기로 작동한다. = state의 값은 setState가 호출되는 시점이 아닌, 해당 코드가 들어있는 함수가 모두 실행된 이후에 바뀐다.
+          => 임시공간에 있는값을 사용하기위해 prev(임시공간) 사용해야 올바른 값이 나옴 */}
           -
         </button>
         <button
